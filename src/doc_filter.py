17e9f36ac22e9ce2c65ac06c7ce25bc39fb9dcc9
Alex Haug <stahh02@moravian.edu>
2018-07-23 15:49:25 -0400

    Finished adding Logging to Doc Filter.

--- src/doc_filter.py
+++ src/doc_filter.py
@@ -54,17 +54,36 @@ def get_doc_attributes(file_name):
              docket_id: the docket_id,
              document_id: the document_id
     """
+
+    logger.warning('Function Successful: % s',
+                   'get_doc_attributes: get_doc_attributes successfully called from process_doc', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'get_doc_attributes: get_doc_attributes calling get_document_id', extra=d)
     document_id = get_document_id(file_name)
+    logger.warning('Function Successful: % s',
+                   'get_doc_attributes: get_doc_attributes successfully called get_document_id', extra=d)
 
     if "_" in document_id:
+        logger.warning('Calling Function: % s',
+                       'get_doc_attributes: get_doc_attributes calling split', extra=d)
         split_name = re.split("[-_]", document_id)
+        logger.warning('Function Successful: % s',
+                       'get_doc_attributes: get_doc_attributes successfully called split', extra=d)
+
         org = split_name[0] + "_" + split_name[1]
         docket_id = org + "_" + split_name[2]
         document_id = docket_id + "-" + split_name[3]
+        logger.warning('Returning: %s',
+                       'get_doc_attributes: returning the organization, docket_id, and document_id', extra=d)
         return org, docket_id, document_id
 
     else:
+        logger.warning('Calling Function: % s',
+                       'get_doc_attributes: get_doc_attributes calling split', extra=d)
         split_name = re.split("[-]", document_id)
+        logger.warning('Function Successful: % s',
+                       'get_doc_attributes: get_doc_attributes successfully called split', extra=d)
         length = len(split_name)
         count = 0
         for x in range(length):
@@ -75,10 +94,27 @@ def get_doc_attributes(file_name):
 
         org_list = split_name[:count]
         org_list.sort()
-        org = add_hyphens(org_list)
-        docket_id = add_hyphens(split_name[:len(split_name) - 1])
-        document_id = add_hyphens(split_name[:len(split_name)])
 
+        logger.warning('Calling Function: % s',
+                       'get_doc_attributes: get_doc_attributes calling add_hyphens', extra=d)
+        org = add_hyphens(org_list)
+        logger.warning('Function Successful: % s',
+                       'get_doc_attributes: get_doc_attributes successfully called add_hyphens', extra=d)
+
+        logger.warning('Calling Function: % s',
+                       'get_doc_attributes: get_doc_attributes calling add_hyphens', extra=d)
+        docket_id = add_hyphens(split_name[:len(split_name) - 1])
+        logger.warning('Function Successful: % s',
+                       'get_doc_attributes: get_doc_attributes successfully called add_hyphens', extra=d)
+
+        logger.warning('Calling Function: % s',
+                       'get_doc_attributes: get_doc_attributes calling add_hyphens', extra=d)
+        document_id = add_hyphens(split_name[:len(split_name)])
+        logger.warning('Function Successful: % s',
+                       'get_doc_attributes: get_doc_attributes successfully called add_hyphens', extra=d)
+
+        logger.warning('Returning: %s',
+                       'get_doc_attributes: returning the organization, docket_id, and document_id', extra=d)
         return org,docket_id,document_id
 
 
@@ -88,6 +124,9 @@ def add_hyphens(list):
     :param list: the list to be hyphenated
     :return: A string of the list with hyphens in-between
     """
+    logger.warning('Function Successful: % s',
+                   'add_hyphens: add_hyphens successfully called from get_doc_attributes', extra=d)
+
     hyphened_string = ""
     for x in range(len(list)):
 
@@ -103,6 +142,8 @@ def add_hyphens(list):
         else:
             hyphened_string = hyphened_string + list[x] + "-"
 
+    logger.warning('Returning: %s',
+                   'add_hyphens: returning the hyphened_string', extra=d)
     return hyphened_string
 
 
@@ -113,8 +154,19 @@ def ending_is_number(document_id):
     :param document_id: the document id being checked
     :return:
     """
+    logger.warning('Function Successful: % s',
+                   'ending_is_number: ending_is_number successfully called from process_doc', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'ending_is_number: ending_is_number calling split', extra=d)
     list = re.split("-", document_id)
+    logger.warning('Function Successful: % s',
+                   'ending_is_number: ending_is_number successfully called split', extra=d)
+
     number = list[-1]
+
+    logger.warning('Returning: %s',
+                   'ending_is_number: returning the number', extra=d)
     return number.isdigit()
 
 
@@ -125,12 +177,32 @@ def id_matches(path, doc_id):
     :param doc_id: the document id to check
     :return:
     """
+    logger.warning('Function Successful: % s',
+                   'id_matches: id_matches successfully called from process_doc', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'id_matches: id_matches calling open', extra=d)
     f = open(path, "r")
+    logger.warning('Function Successful: % s',
+                   'id_matches: id_matches successfully called open', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'id_matches: id_matches calling load', extra=d)
     j = json.load(f)
+    logger.warning('Function Successful: % s',
+                   'id_matches: id_matches successfully called load', extra=d)
 
     document_id = j["documentId"]["value"]
 
-    return document_id == doc_id
+    result = document_id == doc_id
+    if result is True:
+        logger.warning('Returning: %s',
+                       'id_matches: returning True', extra=d)
+        return True
+    else:
+        logger.warning('Returning: %s',
+                       'id_matches: returning False', extra=d)
+        return False
 
 
 def beginning_is_letter(document_id):
@@ -139,8 +211,20 @@ def beginning_is_letter(document_id):
     :param document_id: the document id being checked
     :return:
     """
+    logger.warning('Function Successful: % s',
+                   'beginning_is_letter: beginning_is_letter successfully called from process_doc', extra=d)
+
     letter = document_id[0]
-    return letter.isalpha()
+
+    result = letter.isalpha()
+    if result is True:
+        logger.warning('Returning: %s',
+                       'beginning_is_letter: returning True', extra=d)
+        return True
+    else:
+        logger.warning('Returning: %s',
+                       'beginning_is_letter: returning False', extra=d)
+        return False
 
 
 # Saving Functions
@@ -151,11 +235,34 @@ def local_save(cur_path, destination):
     :param destination: location that the file should be saved
     :return:
     """
+    logger.warning('Function Successful: % s',
+                   'local_save: local_save successfully called from process_doc', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'local_save: local_save calling get_file_name', extra=d)
     file_name = get_file_name(cur_path)
+    logger.warning('Function Successful: % s',
+                   'local_save: local_save successfully called get_file_name', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'local_save: local_save calling get_doc_attributes', extra=d)
     org, docket_id, document_id = get_doc_attributes(file_name)
+    logger.warning('Function Successful: % s',
+                   'local_save: local_save successfully called get_doc_attributes', extra=d)
+
     destination_path = destination + org + "/" + docket_id + "/" + document_id + "/"
+
+    logger.warning('Calling Function: % s',
+                   'local_save: local_save calling create_new_dir', extra=d)
     create_new_dir(destination_path)
+    logger.warning('Function Successful: % s',
+                   'local_save: local_save successfully called create_new_dir', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'local_save: local_save calling copy', extra=d)
     shutil.copy(cur_path, destination_path + '/' + file_name)
+    logger.warning('Function Successful: % s',
+                   'local_save: local_save successfully called copy', extra=d)
 
 
 def create_new_dir(path):
@@ -164,8 +271,15 @@ def create_new_dir(path):
     :param path: the path to the directory to be created
     :return:
     """
+    logger.warning('Function Successful: % s',
+                   'create_new_dir: create_new_dir successfully called from local_save', extra=d)
+
     if not os.path.exists(path):
+        logger.warning('Calling Function: % s',
+                       'create_new_dir: create_new_dir calling makedirs', extra=d)
         os.makedirs(path)
+        logger.warning('Function Successful: % s',
+                       'create_new_dir: create_new_dir successfully called makedirs', extra=d)
 
 
 def get_file_list(compressed_file, PATHstr):
@@ -175,17 +289,35 @@ def get_file_list(compressed_file, PATHstr):
     :param PATHstr: location of the file in string form
     :return: The list of file names in the compressed file
     """
+    logger.warning('Function Successful: % s',
+                   'get_file_list: get_file_list successfully called from process_doc', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'get_file_list: get_file_list calling ZipFile', extra=d)
     files = zipfile.ZipFile(compressed_file, "r")
+    logger.warning('Function Successful: % s',
+                   'get_file_list: get_file_list successfully called ZipFile', extra=d)
+
+    logger.warning('Calling Function: % s',
+                   'get_file_list: get_file_list calling extractall', extra=d)
     files.extractall(PATHstr)
+    logger.warning('Function Successful: % s',
+                   'get_file_list: get_file_list successfully called extractall', extra=d)
 
     # Create a list of all the files in the directory
+    logger.warning('Calling Function: % s',
+                   'get_file_list: get_file_list calling listdir', extra=d)
     file_list = os.listdir(PATHstr)
+    logger.warning('Function Successful: % s',
+                   'get_file_list: get_file_list successfully called listdir', extra=d)
 
     final_list = []
     for file in file_list:
         if file.startswith("doc."):
             final_list.append(file)
 
+    logger.warning('Returning: %s',
+                   'get_file_list: returning list of files', extra=d)
     return final_list
 
 
@@ -197,46 +329,134 @@ def process_doc(json_data, compressed_file):
     :param compressed_file: compressed file of document data
     :return:
     """
+    logger.warning('Function Successful: % s',
+                   'process_doc: process_doc successfully called from return_doc', extra=d)
 
     if r.does_job_exist_in_progress(json_data["job_id"]) is False:
-        pass
+        logger.warning('Variable Failure: %s',
+                       'process_doc: job_id does not exist in progress queue', extra=d)
 
     else:
-
+        logger.warning('Calling Function: % s',
+                       'process_doc: process_doc calling mkdtemp', extra=d)
         PATH = tempfile.mkdtemp()
+        logger.warning('Function Successful: % s',
+                       'process_doc: process_doc successfully called mkdtemp', extra=d)
         PATHstr = str(PATH)
 
         # Unzip the zipfile and then remove the tar file and create a list of all the files in the directory
+        logger.warning('Calling Function: % s',
+                       'process_doc: process_doc calling mkdtemp', extra=d)
         file_list = get_file_list(compressed_file, PATHstr)
+        logger.warning('Function Successful: % s',
+                       'process_doc: process_doc successfully called mkdtemp', extra=d)
 
         renew = False
         for file in file_list:
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling get_doc_attributes', extra=d)
             org, docket_id, document_id = get_doc_attributes(file)
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called get_doc_attributes', extra=d)
 
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling startswith', extra=d)
             fsw = file.startswith("doc.")
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called startswith', extra=d)
+
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling beginning_is_letter', extra=d)
             bil = beginning_is_letter(document_id)
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called beginning_is_letter', extra=d)
+
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling ending_is_number', extra=d)
             ein = ending_is_number(document_id)
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called ending_is_number', extra=d)
+
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling endswith', extra=d)
             few = file.endswith(".json")
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called endswith', extra=d)
+
             job_type = json_data["job_type"] == "doc"
 
             if fsw and bil and ein and few and job_type:
+                logger.warning('Calling Function: % s',
+                               'process_doc: process_doc calling id_matches', extra=d)
                 if id_matches(file, document_id):
-                    pass
+                    logger.warning('Variable Success: %s',
+                                   'process_doc: fsw, bil, ein, and job_type are True', extra=d)
+                    logger.warning('Function Successful: % s',
+                                   'process_doc: process_doc successfully called id_matches', extra=d)
                 else:
+                    logger.warning('Variable Success: %s',
+                                   'process_doc: fsw, bil, ein, and job_type are True', extra=d)
+                    logger.warning('Function Successful: % s',
+                                   'process_doc: process_doc successfully called id_matches', extra=d)
+                    logger.warning('Variable Failure: %s',
+                                   'process_doc: id_matches is False', extra=d)
                     renew = True
                     break
 
             elif fsw and bil and ein and job_type:
-                    pass
+                logger.warning('Variable Success: %s',
+                               'process_doc: fsw, bil, ein, and job_type are True', extra=d)
             else:
+                if fsw is False:
+                    logger.warning('Variable Failure: %s',
+                                   'process_doc: fsw is False', extra=d)
+                else:
+                    logger.warning('Variable Success: %s',
+                                   'process_doc: fsw is True', extra=d)
+                if bil is False:
+                    logger.warning('Variable Failure: %s',
+                                   'process_doc: bil is False', extra=d)
+                else:
+                    logger.warning('Variable Success: %s',
+                                   'process_doc: bil is True', extra=d)
+                if ein is False:
+                    logger.warning('Variable Failure: %s',
+                                   'process_doc: ein is False', extra=d)
+                else:
+                    logger.warning('Variable Success: %s',
+                                   'process_doc: ein is True', extra=d)
+                if job_type is False:
+                    logger.warning('Variable Failure: %s',
+                                   'process_doc: job_type is not doc', extra=d)
+                else:
+                    logger.warning('Variable Success: %s',
+                                   'process_doc: job_type is doc', extra=d)
                 renew = True
                 break
 
         if renew is True:
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling renew_job', extra=d)
             r.renew_job(json_data)
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called renew_job', extra=d)
 
         else:
             for file in file_list:
+                logger.warning('Calling Function: % s',
+                               'process_doc: process_doc calling local_save', extra=d)
                 local_save(PATHstr + "/" + file, "~/regulations-data/")
+                logger.warning('Function Successful: % s',
+                               'process_doc: process_doc successfully called local_save', extra=d)
+
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling get_keys_from_progress', extra=d)
             key = r.get_keys_from_progress(json_data["job_id"])
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called get_keys_from_progress', extra=d)
+
+            logger.warning('Calling Function: % s',
+                           'process_doc: process_doc calling remove_job_from_progress', extra=d)
             r.remove_job_from_progress(key)
+            logger.warning('Function Successful: % s',
+                           'process_doc: process_doc successfully called remove_job_from_progress', extra=d)
