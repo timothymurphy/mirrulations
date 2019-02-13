8a30021c88097b86b7aa51e18066d1bbaa4c6ab7
Alex Haug <stahh02@moravian.edu>
2019-02-13 13:47:43 -0500

    Added testing directory and testing method

--- src/dir_search.py
+++ src/dir_search.py
@@ -12,24 +12,38 @@ PATH = os.getenv("HOME")+"/regulations_data/"
 
 
 def search_for_document(document_id):
+    """
+    Called by the server to check to see if a document exists in the directory structure
+    :param document_id:
+    :return: Return the full path if the document exists, else return an empty string
+    """
     orgs, dock_id, doc_id = df.get_doc_attributes(document_id)
 
     full_path = PATH + orgs + "/" + dock_id + "/" + doc_id
+    doc_json = "doc." + doc_id + ".json"
 
-    if os.path.isfile(full_path + "/doc."+doc_id+".json"):
+    if os.path.isfile(full_path + "/" + doc_json):
         return full_path
     else:
         return ""
 
 
-def search_for_document_temp(document_id, dir):
+def search_for_document_test_directory(document_id, dir):
+    """
+    Same as the search_for_document.
+    This is only to be used for testing purposes.
+    :param document_id:
+    :param dir: The test directory being used
+    :return: Return the full path if the document exists, else return an empty string
+    """
     orgs, dock_id, doc_id = df.get_doc_attributes(document_id)
 
     full_path = dir + orgs + "/" + dock_id + "/" + doc_id
+    doc_json = "doc." + doc_id + ".json"
 
     print("This is the full temp path:" + full_path)
 
-    if os.path.isfile(full_path + "/doc."+doc_id+".json"):
+    if os.path.isfile(full_path + "/" + doc_json):
         return full_path
     else:
         return ""
--- tests/test_dir_search.py
+++ tests/test_dir_search.py
@@ -3,14 +3,20 @@ import os
 import dir_search as ds
 
 PATH = os.getenv("HOME")+"/regulations_data/"
+TEMPATH = os.getenv("HOME")+"/mirrulations/tests/test_files/regulations-data/"
 
 
 def test_search_document():
+    "This test requires the regulations-data directory structure on the testing machine"
     full_path = ds.search_for_document("AHRQ_FRDOC_0001-0036")
     assert full_path == PATH + "AHRQ_FRDOC/AHRQ_FRDOC_0001/AHRQ_FRDOC_0001-0036"
 
 
-def dtest_search_document_temp():
-    PATHstr = "/test_files/regulations-data/"
-    full_path = ds.search_for_document_temp("AHRQ_FRDOC_0001-0036", PATHstr)
-    assert full_path == PATHstr
+def test_search_document_test_directory_good_document():
+    full_path = ds.search_for_document_test_directory("AHRQ_FRDOC_0001-0036", TEMPATH)
+    assert full_path == TEMPATH + "AHRQ_FRDOC/AHRQ_FRDOC_0001/AHRQ_FRDOC_0001-0036"
+
+
+def test_search_document_test_directory_bad_document():
+    full_path = ds.search_for_document_test_directory("AHRQ_FRDOC_0001-0037", TEMPATH)
+    assert full_path == ""
