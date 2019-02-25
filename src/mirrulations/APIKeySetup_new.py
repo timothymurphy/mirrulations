0d4a15d8bf0678c3f538020b688b3c1a340944b2
edwharbison <edwharbison@gmail.com>
2019-02-25 10:50:22 -0500

    Make popup window

--- src/mirrulations/APIKeySetup_new.py
+++ src/mirrulations/APIKeySetup_new.py
@@ -12,20 +12,46 @@ from pathlib import Path
  API Key, and generate a random ClientID.
 '''
 
-app = gui('Mirrulations Login', geom='300x50')
+with gui('Mirrulations Login') as app:
 
-app.addLabel('IPv4 Address', text='IPv4 Address:', column=0, row=0)
-app.addNumericEntry('IP1', column=1, row=0)
-app.addLabel('.1', text='.', column=2, row=0)
-app.addNumericEntry('IP2', column=3, row=0)
-app.addLabel('.2', text='.', column=4, row=0)
-app.addNumericEntry('IP3', column=5, row=0)
-app.addLabel('.3', text='.', column=6, row=0)
-app.addNumericEntry('IP4', column=7, row=0)
-app.addLabel('.4', text=':', column=8, row=0)
-app.addNumericEntry('Port', column=9, row=0)
-app.setEntryDefault('Port', 'Port')
-app.addLabel('API Key:', column=0, row=1)
-app.addEntry('API Key', column=1, row=1, colspan=9)
+    app.setSize('500x100')
+    app.setFont(size=20, family="Gill Sans")
 
-app.go()
+    app.addLabel('IPv4 Address', text='IPv4 Address:', column=0, row=0)
+    app.setLabelWidth('IPv4 Address', 20)
+
+    app.addNumericEntry('IP1', column=1, row=0)
+    app.setEntryWidth('IP1', 12)
+
+    app.addLabel('.1', text='.', column=2, row=0)
+    app.setLabelWidth('.1', 4)
+
+    app.addNumericEntry('IP2', column=3, row=0)
+    app.setEntryWidth('IP2', 12)
+
+    app.addLabel('.2', text='.', column=4, row=0)
+    app.setLabelWidth('.2', 4)
+
+    app.addNumericEntry('IP3', column=5, row=0)
+    app.setEntryWidth('IP3', 12)
+
+    app.addLabel('.3', text='.', column=6, row=0)
+    app.setLabelWidth('.3', 4)
+
+    app.addNumericEntry('IP4', column=7, row=0)
+    app.setEntryWidth('IP4', 12)
+
+    app.addLabel('.4', text=':', column=8, row=0)
+    app.setLabelWidth('.4', 4)
+
+    app.addNumericEntry('Port', column=9, row=0)
+    app.setEntryWidth('Port', 16)
+    app.setEntryDefault('Port', 'Port')
+
+    app.addLabel('API Key:', column=0, row=1)
+    app.addEntry('API Key', column=1, row=1, colspan=9)
+
+    app.addButton('Submit', None, column=1, row=2, colspan=3)
+    app.addButton('Cancel', None, column=5, row=2, colspan=3)
+
+    app.go()
