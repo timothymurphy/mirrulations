345421df8900f8f41035199023d6e46b2612f085
Alex Haug <stahh02@moravian.edu>
2019-03-01 12:24:02 -0500

    Added Mirrulations Core package

--- src/mirrulations/doc_filter.py
+++ src/mirrulations/doc_filter.py
@@ -6,6 +6,7 @@ import shutil
 import re
 import zipfile
 import logging
+import mirrulations_core.documents_core as dc
 
 FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
 logging.basicConfig(filename='doc_filter.log', format=FORMAT)
@@ -54,104 +55,6 @@ def get_file_name(path):
     logger.info('File name extracted')
 
     return file_name
-
-
-def get_doc_attributes(document_id):
-    """
-    Get the organization(s), the docket_id and the document_id from a file name
-    :param file_name: name of the file to extract attributes of the document name
-    :return: orgs: The organizations(s),
-             docket_id: the docket_id,
-             document_id: the document_id
-    """
-
-    logger.debug('Function Successful: % s',
-                   'get_doc_attributes: get_doc_attributes successfully called from process_doc', extra=d)
-
-    if "_" in document_id:
-        logger.debug('Calling Function: % s',
-                       'get_doc_attributes: get_doc_attributes calling split', extra=d)
-        split_name = re.split("[-_]", document_id)
-        logger.debug('Function Successful: % s',
-                       'get_doc_attributes: get_doc_attributes successfully called split', extra=d)
-
-        org = split_name[0] + "_" + split_name[1]
-        docket_id = org + "_" + split_name[2]
-        document_id = docket_id + "-" + split_name[3]
-        logger.debug('Returning: %s',
-                       'get_doc_attributes: returning the organization, docket_id, and document_id', extra=d)
-        return org, docket_id, document_id
-
-    else:
-        logger.debug('Calling Function: % s',
-                       'get_doc_attributes: get_doc_attributes calling split', extra=d)
-        split_name = re.split("[-]", document_id)
-        logger.debug('Function Successful: % s',
-                       'get_doc_attributes: get_doc_attributes successfully called split', extra=d)
-        length = len(split_name)
-        count = 0
-        for x in range(length):
-            if split_name[x].isdigit():
-                break
-            else:
-                count += 1
-
-        org_list = split_name[:count]
-        org_list.sort()
-
-        logger.debug('Calling Function: % s',
-                       'get_doc_attributes: get_doc_attributes calling add_hyphens', extra=d)
-        org = add_hyphens(org_list)
-        logger.debug('Function Successful: % s',
-                       'get_doc_attributes: get_doc_attributes successfully called add_hyphens', extra=d)
-
-        logger.debug('Calling Function: % s',
-                       'get_doc_attributes: get_doc_attributes calling add_hyphens', extra=d)
-        docket_id = add_hyphens(split_name[:len(split_name) - 1])
-        logger.debug('Function Successful: % s',
-                       'get_doc_attributes: get_doc_attributes successfully called add_hyphens', extra=d)
-
-        logger.debug('Calling Function: % s',
-                       'get_doc_attributes: get_doc_attributes calling add_hyphens', extra=d)
-        document_id = add_hyphens(split_name[:len(split_name)])
-        logger.debug('Function Successful: % s',
-                       'get_doc_attributes: get_doc_attributes successfully called add_hyphens', extra=d)
-
-        logger.debug('Returning: %s',
-                       'get_doc_attributes: returning the organization, docket_id, and document_id', extra=d)
-        return org,docket_id,document_id
-
-
-def add_hyphens(list):
-    """
-    Adds hyphens between the list of strings passed
-    :param list: the list to be hyphenated
-    :return: A string of the list with hyphens in-between
-    """
-    logger.debug('Function Successful: % s',
-                   'add_hyphens: add_hyphens successfully called from get_doc_attributes', extra=d)
-    logger.info('Hyphenating strings...')
-
-    hyphened_string = ""
-    for x in range(len(list)):
-
-        if x == 0:
-            if len(list) == 1:
-                hyphened_string = list[x]
-            else:
-                hyphened_string = list[x] + "-"
-
-        elif x == len(list) - 1:
-            hyphened_string = hyphened_string + list[x]
-
-        else:
-            hyphened_string = hyphened_string + list[x] + "-"
-
-    logger.debug('Returning: %s',
-                   'add_hyphens: returning the hyphened_string', extra=d)
-    logger.info('Strings hyphenated')
-
-    return hyphened_string
 
 
 # Validation Functions
@@ -262,7 +165,7 @@ def save_single_file_locally(cur_path, destination):
     logger.debug('Calling Function: % s',
                    'save_single_file_locally: save_single_file_locally calling get_doc_attributes', extra=d)
     doc_id = get_document_id(file_name)
-    org, docket_id, document_id = get_doc_attributes(doc_id)
+    org, docket_id, document_id = dc.get_doc_attributes(doc_id)
     logger.debug('Function Successful: % s',
                    'save_single_file_locally: save_single_file_locally successfully called get_doc_attributes', extra=d)
 
@@ -383,7 +286,7 @@ def process_doc(redis_server, json_data, compressed_file):
             save_all_files_locally(file_list, path)
             logger.debug('Calling Function: % s',
                          'process_doc: process_doc calling remove_job_from_progress', extra=d)
-            remove_job(json_data)
+            remove_job(redis_server, json_data)
             logger.debug('Function Successful: % s',
                          'process_doc: process_doc successfully called remove_job_from_progress', extra=d)
 
@@ -396,7 +299,7 @@ def check_single_document(file, json_data, path):
     :param path:
     :return:
     """
-    org, docket_id, document_id = get_doc_attributes(file)
+    org, docket_id, document_id = dc.get_doc_attributes(file)
     ifFileStartsWithDoc = file.startswith("doc.")
     ifBeginWithDocLetter = beginning_is_letter(document_id)
     ifEndIsDocNum = ending_is_number(document_id)
