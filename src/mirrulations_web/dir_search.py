2a1f8d3d5a0c86551c1a30f6920e563f2e88efeb
Alex Haug <stahh02@moravian.edu>
2019-03-01 12:57:28 -0500

    Refactored dirsearch

--- src/mirrulations_web/dir_search.py
+++ src/mirrulations_web/dir_search.py
@@ -8,27 +8,7 @@ d = { 'clientip': '192.168.0.1', 'user': 'FILTERS'}
 logger = logging.getLogger('tcpserver')
 
 
-PATH = os.getenv("HOME")+"/regulations_data/"
-
-
-def search_for_document(document_id):
-    """
-    Called by the server to check to see if a document exists in the directory structure
-    :param document_id:
-    :return: Return the full path if the document exists, else return an empty string
-    """
-    orgs, dock_id, doc_id = dc.get_doc_attributes(document_id)
-
-    full_path = PATH + orgs + "/" + dock_id + "/" + doc_id
-    doc_json = "doc." + doc_id + ".json"
-
-    if os.path.isfile(full_path + "/" + doc_json):
-        return full_path
-    else:
-        return ""
-
-
-def search_for_document_test_directory(document_id, dir):
+def search_for_document_in_directory(document_id, dir):
     """
     Same as the search_for_document.
     This is only to be used for testing purposes.
--- tests/mirrulations_web_tests/test_dirsearch.py
+++ tests/mirrulations_web_tests/test_dirsearch.py
@@ -12,30 +12,30 @@ TEMPATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../tests/
 
 
 def test_search_document_test_directory_good_document():
-    full_path = ds.search_for_document_test_directory("CMS-2019-0006-10896", TEMPATH)
+    full_path = ds.search_for_document_in_directory("CMS-2019-0006-10896", TEMPATH)
     assert full_path == TEMPATH + "CMS/CMS-2019-0006/CMS-2019-0006-10896"
 
 
 def test_search_document_test_directory_good_document_special_case():
-    full_path = ds.search_for_document_test_directory("AHRQ_FRDOC_0001-0036", TEMPATH)
+    full_path = ds.search_for_document_in_directory("AHRQ_FRDOC_0001-0036", TEMPATH)
     assert full_path == TEMPATH + "AHRQ_FRDOC/AHRQ_FRDOC_0001/AHRQ_FRDOC_0001-0036"
 
 
 def test_search_document_test_directory_good_document_orgs_out_of_order():
-    full_path = ds.search_for_document_test_directory("USC-RULES-CV-2018-0003-1139", TEMPATH)
+    full_path = ds.search_for_document_in_directory("USC-RULES-CV-2018-0003-1139", TEMPATH)
     assert full_path == TEMPATH + "CV-RULES-USC/USC-RULES-CV-2018-0003/USC-RULES-CV-2018-0003-1139"
 
 
 def test_search_document_test_directory_bad_document():
-    full_path = ds.search_for_document_test_directory("CMS-2019-0006-10898", TEMPATH)
+    full_path = ds.search_for_document_in_directory("CMS-2019-0006-10898", TEMPATH)
     assert full_path == ""
 
 
 def test_search_document_test_directory_bad_document_special_case():
-    full_path = ds.search_for_document_test_directory("AHRQ_FRDOC_0001-0037", TEMPATH)
+    full_path = ds.search_for_document_in_directory("AHRQ_FRDOC_0001-0037", TEMPATH)
     assert full_path == ""
 
 
 def test_search_document_test_directory_bad_document_orgs_out_of_order():
-    full_path = ds.search_for_document_test_directory("USC-RULES-CV-2018-0003-1130", TEMPATH)
+    full_path = ds.search_for_document_in_directory("USC-RULES-CV-2018-0003-1130", TEMPATH)
     assert full_path == ""
