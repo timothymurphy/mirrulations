5e703e5e9cf5b134502bd4012a44c58703b3d8a2
Alex Haug <stahh02@moravian.edu>
2019-03-01 13:06:13 -0500

    Updated documentation

--- src/mirrulations_web/dir_search.py
+++ src/mirrulations_web/dir_search.py
@@ -8,17 +8,16 @@ d = { 'clientip': '192.168.0.1', 'user': 'FILTERS'}
 logger = logging.getLogger('tcpserver')
 
 
-def search_for_document_in_directory(document_id, dir):
+def search_for_document_in_directory(document_id, directory_path):
     """
-    Same as the search_for_document.
-    This is only to be used for testing purposes.
-    :param document_id:
-    :param dir: The test directory being used
+    Looks in a directory structure to see if a given document id exists
+    :param document_id: the document id being searched
+    :param directory_path: The directory path being searched in
     :return: Return the full path if the document exists, else return an empty string
     """
     orgs, dock_id, doc_id = dc.get_doc_attributes(document_id)
 
-    full_path = dir + orgs + "/" + dock_id + "/" + doc_id
+    full_path = directory_path + orgs + "/" + dock_id + "/" + doc_id
     doc_json = "doc." + doc_id + ".json"
 
     if os.path.isfile(full_path + "/" + doc_json):
