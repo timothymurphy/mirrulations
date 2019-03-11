b68488e1f226b689ec4c3fba652bac7d6fada3bf
edwharbison <edwharbison@gmail.com>
2019-03-11 15:19:32 -0400

    Update to contain more keywords

--- setup.py
+++ setup.py
@@ -2,6 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='mirrulations',
+    version='0.0.0',
+    python_requires='>=3.7.2',
+    description='The objective of the Mirrulations project is to make the data on Regulations.gov more easily'
+                'accessible to the public by acting as a mirror to the site.',
+    long_description='Read README.md',
+    author='Moravian College',
+    author_email='colemanb@moravian.edu',
+    url='https://github.com/MoravianCollege/mirrulations',
     packages=find_packages('src'),
     package_dir={'': 'src'}
 )
