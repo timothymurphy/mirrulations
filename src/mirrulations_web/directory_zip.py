2a59adb6abbf7070bd9f0048726d5900da3feff2
Alex Haug <stahh02@moravian.edu>
2019-03-11 13:01:01 -0400

    Commiting to create new testing structure

--- src/mirrulations_web/directory_zip.py
+++ src/mirrulations_web/directory_zip.py
@@ -8,9 +8,10 @@ home = os.getenv("HOME")
 
 
 def zip_directory(document_id, directory_path):
-    archive_name = document_id+'.zip'
+    archive_name = document_id +'.zip'
     # root_dir parameter sets the directory to save without causing tarbombing
     # 4th parameter is what directory you are saving
+    # TODO: Try adding a path along with the document id
     shutil.make_archive(document_id, "zip", root_dir=directory_path, base_dir=directory_path)
     add_readme(archive_name, document_id)
     return archive_name
