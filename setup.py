da3da9a6b77ffa92de0a1843e0e3e269f6e6936f
edwharbison <edwharbison@gmail.com>
2019-03-15 22:01:18 -0400

    Allow test install vs normal install

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from setuptools import setup, find_packages
 
+INSTALL_REQUIRES = ['flask==1.0.2',
+                    'redis==3.1.0',
+                    'python-redis-lock==3.3.1',
+                    'requests==2.21.0']
+TESTS_REQUIRE = ['pytest==4.3.0',
+                 'fakeredis[lua]==1.0.2',
+                 'mock==2.0.0',
+                 'requests-mock==1.5.2']
+
 setup(
     name='mirrulations',
     version='0.0.0',
@@ -10,14 +19,9 @@ setup(
     author_email='colemanb@moravian.edu',
     url='https://github.com/MoravianCollege/mirrulations',
     python_requires='>=3.7.2',
-    install_requires=['flask==1.0.2',
-                      'pytest==4.3.0',
-                      'mock==2.0.0',
-                      'redis==3.1.0',
-                      'fakeredis[lua]==1.0.2',
-                      'python-redis-lock==3.3.1',
-                      'requests==2.21.0',
-                      'requests-mock==1.5.2'],
+    install_requires=INSTALL_REQUIRES,
+    tests_require=TESTS_REQUIRE,
+    extras_require={'test': TESTS_REQUIRE},
     packages=find_packages('src'),
     package_dir={'': 'src'},
     entry_points={'console_scripts': ['mirrulations=mirrulations_core.__main__:main']}
