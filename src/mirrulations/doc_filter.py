e82daf9823352691fd19b36b89c2ef5653d637f4
Alex Haug <stahh02@moravian.edu>
2019-03-20 14:42:27 -0400

    Finihsed reorganizing Doc Filter and it's tests

--- src/mirrulations/doc_filter.py
+++ src/mirrulations/doc_filter.py
@@ -18,7 +18,120 @@ This program does the validation of data from the doc jobs and then saves that d
 """
 
 
-# General Functions
+def process_doc(redis_server, json_data, compressed_file):
+    """
+    Main document function, called by the server to check and save document files returned from the client
+    """
+    logger.debug('FILTER JOB_ID: %s', 'process_doc: ' + str(json_data["job_id"]), extra=d)
+    if redis_server.does_job_exist_in_progress(json_data["job_id"]) is False:
+        logger.debug('Variable Failure: %s', 'process_doc: job_id does not exist in progress queue', extra=d)
+
+    else:
+
+        temp_directory = tempfile.mkdtemp()
+        temp_directory_path = str(temp_directory + '/')
+
+        # Unzip the zipfile and then remove the tar file and create a list of all the files in the directory
+        file_list, temp_directory_path = get_file_list(compressed_file, temp_directory_path, json_data['client_id'])
+
+        for file in file_list:
+            JobNeedsRenew = check_if_document_needs_renew(file, json_data, temp_directory_path)
+            if JobNeedsRenew is True:
+                logger.debug('Calling Function: % s', 'process_doc: process_doc calling renew_job', extra=d)
+                redis_server.renew_job(json_data)
+                logger.debug('Function Successful: % s', 'process_doc: process_doc successfully called renew_job',
+                             extra=d)
+
+        else:
+            save_all_files_locally(file_list, temp_directory_path)
+            logger.debug('Calling Function: % s', 'process_doc: process_doc calling remove_job_from_progress', extra=d)
+            remove_job_from_progress(redis_server, json_data)
+            logger.debug('Function Successful: % s', 'process_doc: process_doc successfully called '
+                                                     'remove_job_from_progress', extra=d)
+
+
+def get_file_list(compressed_file, compressed_file_path, client_id):
+    """
+    Get the list of files to be processed from a compressed file
+    :param compressed_file: file containing file list to be uncompressed
+    :param compressed_file_path: location of the file in string form
+    :param client_id: the id of the client that did the job
+    :return: The list of file names in the compressed file
+    """
+
+    client_path = os.getenv("HOME") + '/client-logs/' + str(client_id) + '/'
+
+    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called from process_doc',
+                 extra=d)
+    logger.debug('Calling Function: % s', 'get_file_list: get_file_list calling ZipFile', extra=d)
+
+    files = zipfile.ZipFile(compressed_file, "r")
+
+    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called ZipFile', extra=d)
+    logger.debug('Calling Function: % s', 'get_file_list: get_file_list calling extractall', extra=d)
+
+    files.extractall(compressed_file_path)
+
+    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called extractall', extra=d)
+    logger.debug('Calling Function: % s', 'get_file_list: get_file_list calling listdir', extra=d)
+
+    file_list = os.listdir(compressed_file_path)
+
+    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called listdir', extra=d)
+
+    final_list = []
+    logger.debug('Loop: %s', 'get_file_list: loop through the files', extra=d)
+    for file in file_list:
+        if file.startswith("doc."):
+            final_list.append(file)
+        elif file.endswith(".log"):
+            if not os.path.exists(client_path):
+                os.makedirs(client_path)
+                shutil.copy(compressed_file_path + file, client_path)
+            else:
+                shutil.copy(compressed_file_path + file, client_path)
+
+    logger.debug('Loop successful: %s', 'get_file_list: successfully looped through the files', extra=d)
+    logger.debug('Returning: %s', 'get_file_list: returning list of files', extra=d)
+
+    return final_list, compressed_file_path
+
+
+def check_if_document_needs_renew(file, json_data, path):
+    """
+    Checks to see if a document conforms to our naming conventions
+    """
+    file_name = get_document_id(file)
+    org, docket_id, document_id = dc.get_doc_attributes(file_name)
+
+    FileStartsWithDoc = file.startswith('doc.')
+    FileBeginWithDocLetter = document_id_beginning_is_letter(document_id)
+    FileEndIsDocNum = document_id_ending_is_number(document_id)
+    FileEndsWithJson = file.endswith('.json')
+    FileJobTypeIsDoc = json_data['type'] == 'doc'
+
+    FileCombinedCheck = FileStartsWithDoc and FileBeginWithDocLetter and FileEndIsDocNum and FileJobTypeIsDoc
+    FileCombinedCheckAndJson = FileCombinedCheck and FileEndsWithJson
+
+    if FileCombinedCheckAndJson:
+        if document_id_matches_json_id(path + file, document_id):
+            logger.debug('Variable Success: %s', 'process_doc: FileStartsWithDoc, FileBeginWithDocLetter, '
+                                                 'FileEndIsDocNum, and FileJobTypeIsDoc are True', extra=d)
+            return False
+        else:
+            logger.debug('Variable Failure: %s', 'process_doc: document_id_matches_json_id is False', extra=d)
+            return True
+
+    checks_names = ['FileBeginWithDocLetter', 'FileEndIsDocNum', 'FileStartsWithDoc', 'FileJobTypeIsDoc']
+    checks_values = [FileBeginWithDocLetter, FileEndIsDocNum, FileStartsWithDoc, FileJobTypeIsDoc]
+    dc.write_multiple_checks_into_logger(checks_names, checks_values, 'check_if_document_needs_renew')
+
+    if FileCombinedCheck:
+        return False
+    else:
+        return True
+
+
 def get_document_id(file_name):
 
     logger.debug('Function Successful: % s', 'get_document_id: get_document_id successfully called from '
@@ -33,22 +146,21 @@ def get_document_id(file_name):
     return document_id
 
 
-def get_file_name(file_path):
+def document_id_beginning_is_letter(document_id):
 
-    logger.debug('Function Successful: % s', 'get_file_name: get_file_name successfully called from local_save',
-                 extra=d)
-    logger.info('Extracting file name...')
+    logger.debug('Function Successful: % s', 'document_id_beginning_is_letter: document_id_beginning_is_letter '
+                                             'successfully called from process_doc', extra=d)
+    logger.info('Ensuring that document ID begins with a letter...')
 
-    split_path = file_path.split('/')
-    file_name = split_path[len(split_path) - 1]
+    letter = document_id[0]
+    result = letter.isalpha()
+    result_name = ['result']
+    result_value = [result]
+    dc.write_multiple_checks_into_logger(result_name, result_value, 'document_id_beginning_is_letter')
 
-    logger.debug('Returning: %s', 'get_file_name: returning the file name', extra=d)
-    logger.info('File name extracted')
-
-    return file_name
+    return result
 
 
-# Validation Functions
 def document_id_ending_is_number(document_id):
 
     logger.debug('Function Successful: % s', 'document_id_ending_is_number: document_id_ending_is_number successfully '
@@ -99,22 +211,14 @@ def document_id_matches_json_id(path, doc_id):
     return result
 
 
-def document_id_beginning_is_letter(document_id):
+def save_all_files_locally(file_list, path):
 
-    logger.debug('Function Successful: % s', 'document_id_beginning_is_letter: document_id_beginning_is_letter '
-                                             'successfully called from process_doc', extra=d)
-    logger.info('Ensuring that document ID begins with a letter...')
-
-    letter = document_id[0]
-    result = letter.isalpha()
-    result_name = ['result']
-    result_value = [result]
-    dc.write_multiple_checks_into_logger(result_name, result_value, 'document_id_beginning_is_letter')
-
-    return result
+    for file in file_list:
+        reg_data_path = os.getenv('HOME') + '/regulations-data/'
+        file_path = path + file
+        save_single_file_locally(file_path, reg_data_path)
 
 
-# Saving Functions
 def save_single_file_locally(current_path, destination):
 
     logger.debug('Function Successful: % s', 'save_single_file_locally: save_single_file_locally successfully called '
@@ -150,6 +254,21 @@ def save_single_file_locally(current_path, destination):
                                              'copy', extra=d)
 
 
+def get_file_name(file_path):
+
+    logger.debug('Function Successful: % s', 'get_file_name: get_file_name successfully called from local_save',
+                 extra=d)
+    logger.info('Extracting file name...')
+
+    split_path = file_path.split('/')
+    file_name = split_path[len(split_path) - 1]
+
+    logger.debug('Returning: %s', 'get_file_name: returning the file name', extra=d)
+    logger.info('File name extracted')
+
+    return file_name
+
+
 def create_new_directory_for_path(path):
 
     logger.debug('Function Successful: % s', 'create_new_directory_for_path: create_new_directory_for_path '
@@ -162,130 +281,7 @@ def create_new_directory_for_path(path):
                                                  'successfully called makedirs', extra=d)
 
 
-def get_file_list(compressed_file, compressed_file_path, client_id):
-    """
-    Get the list of files to be processed from a compressed file
-    :param compressed_file: file containing file list to be uncompressed
-    :param compressed_file_path: location of the file in string form
-    :param client_id: the id of the client that did the job
-    :return: The list of file names in the compressed file
-    """
-
-    client_path = os.getenv("HOME") + '/client-logs/' + str(client_id) + '/'
-
-    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called from process_doc',
-                 extra=d)
-    logger.debug('Calling Function: % s', 'get_file_list: get_file_list calling ZipFile', extra=d)
-
-    files = zipfile.ZipFile(compressed_file, "r")
-
-    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called ZipFile', extra=d)
-    logger.debug('Calling Function: % s', 'get_file_list: get_file_list calling extractall', extra=d)
-
-    files.extractall(compressed_file_path)
-
-    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called extractall', extra=d)
-    logger.debug('Calling Function: % s', 'get_file_list: get_file_list calling listdir', extra=d)
-
-    file_list = os.listdir(compressed_file_path)
-
-    logger.debug('Function Successful: % s', 'get_file_list: get_file_list successfully called listdir', extra=d)
-
-    final_list = []
-    logger.debug('Loop: %s', 'get_file_list: loop through the files', extra=d)
-    for file in file_list:
-        if file.startswith("doc."):
-            final_list.append(file)
-        elif file.endswith(".log"):
-            if not os.path.exists(client_path):
-                os.makedirs(client_path)
-                shutil.copy(compressed_file_path + file, client_path)
-            else:
-                shutil.copy(compressed_file_path + file, client_path)
-
-    logger.debug('Loop successful: %s', 'get_file_list: successfully looped through the files', extra=d)
-    logger.debug('Returning: %s', 'get_file_list: returning list of files', extra=d)
-
-    return final_list, compressed_file_path
-
-
-# Final Function
-def process_doc(redis_server, json_data, compressed_file):
-    """
-    Main document function, called by the server to check and save document files returned from the client
-    """
-    logger.debug('FILTER JOB_ID: %s', 'process_doc: ' + str(json_data["job_id"]), extra=d)
-    if redis_server.does_job_exist_in_progress(json_data["job_id"]) is False:
-        logger.debug('Variable Failure: %s', 'process_doc: job_id does not exist in progress queue', extra=d)
-
-    else:
-
-        temp_directory = tempfile.mkdtemp()
-        temp_directory_path = str(temp_directory + '/')
-
-        # Unzip the zipfile and then remove the tar file and create a list of all the files in the directory
-        file_list, temp_directory_path = get_file_list(compressed_file, temp_directory_path, json_data['client_id'])
-
-        for file in file_list:
-            JobNeedsRenew = check_if_document_needs_renew(file, json_data, temp_directory_path)
-            if JobNeedsRenew is True:
-                logger.debug('Calling Function: % s', 'process_doc: process_doc calling renew_job', extra=d)
-                redis_server.renew_job(json_data)
-                logger.debug('Function Successful: % s', 'process_doc: process_doc successfully called renew_job',
-                             extra=d)
-
-        else:
-            save_all_files_locally(file_list, temp_directory_path)
-            logger.debug('Calling Function: % s', 'process_doc: process_doc calling remove_job_from_progress', extra=d)
-            remove_job_from_progress(redis_server, json_data)
-            logger.debug('Function Successful: % s', 'process_doc: process_doc successfully called '
-                                                     'remove_job_from_progress', extra=d)
-
-
-def check_if_document_needs_renew(file, json_data, path):
-    """
-    Checks to see if a document conforms to our naming conventions
-    """
-    file_name = get_document_id(file)
-    org, docket_id, document_id = dc.get_doc_attributes(file_name)
-
-    FileStartsWithDoc = file.startswith('doc.')
-    FileBeginWithDocLetter = document_id_beginning_is_letter(document_id)
-    FileEndIsDocNum = document_id_ending_is_number(document_id)
-    FileEndsWithJson = file.endswith('.json')
-    FileJobTypeIsDoc = json_data['type'] == 'doc'
-
-    FileCombinedCheck = FileStartsWithDoc and FileBeginWithDocLetter and FileEndIsDocNum and FileJobTypeIsDoc
-    FileCombinedCheckAndJson = FileCombinedCheck and FileEndsWithJson
-
-    if FileCombinedCheckAndJson:
-        if document_id_matches_json_id(path + file, document_id):
-            logger.debug('Variable Success: %s', 'process_doc: FileStartsWithDoc, FileBeginWithDocLetter, '
-                                                 'FileEndIsDocNum, and FileJobTypeIsDoc are True', extra=d)
-            return False
-        else:
-            logger.debug('Variable Failure: %s', 'process_doc: document_id_matches_json_id is False', extra=d)
-            return True
-
-    checks_names = ['FileBeginWithDocLetter', 'FileEndIsDocNum', 'FileStartsWithDoc', 'FileJobTypeIsDoc']
-    checks_values = [FileBeginWithDocLetter, FileEndIsDocNum, FileStartsWithDoc, FileJobTypeIsDoc]
-    dc.write_multiple_checks_into_logger(checks_names, checks_values, 'check_if_document_needs_renew')
-
-    if FileCombinedCheck:
-        return False
-    else:
-        return True
-
-
 def remove_job_from_progress(redis_server, json_data):
 
     key = redis_server.get_keys_from_progress(json_data['job_id'])
     redis_server.remove_job_from_progress(key)
-
-
-def save_all_files_locally(file_list, path):
-
-    for file in file_list:
-        reg_data_path = os.getenv('HOME') + '/regulations-data/'
-        file_path = path + file
-        save_single_file_locally(file_path, reg_data_path)
--- tests/mirrulations_tests/test_doc_filter.py
+++ tests/mirrulations_tests/test_doc_filter.py
@@ -5,7 +5,7 @@ import os
 import mirrulations.doc_filter as df
 
 
-PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../tests/test_files/mirrulations_files/")
+PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../tests/test_files/mirrulations_files/')
 
 @pytest.fixture
 def mock_req():
@@ -25,73 +25,23 @@ def savefile_tempdir():
         yield tmpdirname
 
 
-# General Tests
-def test_get_document_id():
-    assert df.get_document_id('doc.mesd-2018-234234-0001.json') == "mesd-2018-234234-0001"
-
-
-def test_get_document_id_special():
-    assert df.get_document_id('doc.AHRQ_FRDOC_0001-0036.json') == "AHRQ_FRDOC_0001-0036"
-
-
-def test_get_document_id_other_special():
-    assert df.get_document_id('doc.FDA-2018-N-0073-0002.json') == "FDA-2018-N-0073-0002"
-
-
-def test_get_file_name():
-    assert df.get_file_name(PATH + 'doc.mesd-2018-234234-0001.json') == "doc.mesd-2018-234234-0001.json"
-
-
-# Validation Tests
-def test_is_document_ending_a_number():
-    assert df.document_id_ending_is_number("FDA-2018-N-0073-0002") is True
-
-
-def test_is_document_ending_a_number_special():
-    assert df.document_id_ending_is_number("AHRQ_FRDOC_0001-0036") is True
-
-
-def test_is_document_ending_a_word():
-    assert df.document_id_ending_is_number("FDA-2018-N-0073-Abcd") is False
-
-
-def test_is_document_ending_a_word_special():
-    assert df.document_id_ending_is_number("AHRQ_FRDOC_0001-WXyz") is False
-
-
-def test_id_matches_good():
-    assert df.document_id_matches_json_id(PATH + "doc.FMCSA-1997-2350-21654.json", "FMCSA-1997-2350-21654") is True
-
-
-def test_id_matches_bad():
-    assert df.document_id_matches_json_id(PATH + "doc.FMCSA-1997-2350-21653.json", "FMCSA-1997-2350-21653") is False
-
-
-def test_is_document_beginning_good():
-    assert df.document_id_beginning_is_letter("AHRQ_FRDOC_0001-0036") is True
-
-
-def test_is_document_beginning_bad():
-    assert df.document_id_beginning_is_letter("9147_FRDOC_0001-0036") is False
-
-
-# Saving Tests
 def test_get_file_list(workfile_tempdir, savefile_tempdir):
-    compressed_file = PATH + "Archive.zip"
+    compressed_file = PATH + 'Archive.zip'
     PATHstr = savefile_tempdir
-    file_list = df.get_file_list(compressed_file, PATHstr + "/", '123')
+    file_list = df.get_file_list(compressed_file, PATHstr + '/', '123')
     assert len(file_list[0]) == 4
 
 
 def test_get_file_list_and_work(workfile_tempdir, savefile_tempdir):
-    compressed_file = PATH + "Archive.zip"
+    compressed_file = PATH + 'Archive.zip'
     PATHstr = savefile_tempdir
-    file_list = df.get_file_list(compressed_file, PATHstr + "/", '123')
+    file_list = df.get_file_list(compressed_file, PATHstr + '/', '123')
 
     condition = True
     for file in file_list[0]:
         document_id = df.get_document_id(file)
-        if file.startswith("doc.") and df.document_id_ending_is_number(document_id) and df.document_id_beginning_is_letter(document_id):
+        if file.startswith('doc.') and df.document_id_ending_is_number(document_id) \
+                and df.document_id_beginning_is_letter(document_id):
             pass
         else:
             condition = False
@@ -101,14 +51,15 @@ def test_get_file_list_and_work(workfile_tempdir, savefile_tempdir):
 
 
 def test_get_file_list_and_bad_work(savefile_tempdir):
-    compressed_file = PATH + "Bad_Archive.zip"
+    compressed_file = PATH + 'Bad_Archive.zip'
     PATHstr = savefile_tempdir
-    file_list = df.get_file_list(compressed_file, PATHstr + "/", '123')
+    file_list = df.get_file_list(compressed_file, PATHstr + '/', '123')
 
     condition = True
     for file in file_list[0]:
         document_id = df.get_document_id(file)
-        if file.startswith("doc.") and df.document_id_ending_is_number(document_id) and df.document_id_beginning_is_letter(document_id):
+        if file.startswith('doc.') and df.document_id_ending_is_number(document_id) \
+                and df.document_id_beginning_is_letter(document_id):
             pass
         else:
             condition = False
@@ -117,14 +68,15 @@ def test_get_file_list_and_bad_work(savefile_tempdir):
 
 
 def test_get_file_list_and_more_bad_work(savefile_tempdir):
-    compressed_file = PATH + "Bad_Middle_Archive.zip"
+    compressed_file = PATH + 'Bad_Middle_Archive.zip'
     PATHstr = savefile_tempdir
-    file_list = df.get_file_list(compressed_file, PATHstr + "/", '123')
+    file_list = df.get_file_list(compressed_file, PATHstr + '/', '123')
 
     condition = True
     for file in file_list[0]:
         document_id = df.get_document_id(file)
-        if file.startswith("doc.") and df.document_id_ending_is_number(document_id) and df.document_id_beginning_is_letter(document_id):
+        if file.startswith('doc.') and df.document_id_ending_is_number(document_id) \
+                and df.document_id_beginning_is_letter(document_id):
             pass
         else:
             condition = False
@@ -133,14 +85,15 @@ def test_get_file_list_and_more_bad_work(savefile_tempdir):
 
 
 def test_get_file_list_and_bad_number_work(savefile_tempdir):
-    compressed_file = PATH + "Bad_Number_Archive.zip"
+    compressed_file = PATH + 'Bad_Number_Archive.zip'
     PATHstr = savefile_tempdir
-    file_list = df.get_file_list(compressed_file, PATHstr + "/", '123')
+    file_list = df.get_file_list(compressed_file, PATHstr + '/', '123')
 
     condition = True
     for file in file_list[0]:
         document_id = df.get_document_id(file)
-        if file.startswith("doc.") and df.document_id_ending_is_number(document_id) and df.document_id_beginning_is_letter(document_id):
+        if file.startswith('doc.') and df.document_id_ending_is_number(document_id) \
+                and df.document_id_beginning_is_letter(document_id):
             pass
         else:
             condition = False
@@ -160,16 +113,63 @@ def test_check_if_document_needs_renew_bad_json():
     assert df.check_if_document_needs_renew('doc.FMCSA-1997-2350-21653.json', {'type': 'doc'}, PATH) is True
 
 
+def test_get_document_id():
+    assert df.get_document_id('doc.mesd-2018-234234-0001.json') == 'mesd-2018-234234-0001'
+
+
+def test_get_document_id_special():
+    assert df.get_document_id('doc.AHRQ_FRDOC_0001-0036.json') == 'AHRQ_FRDOC_0001-0036'
+
+
+def test_get_document_id_other_special():
+    assert df.get_document_id('doc.FDA-2018-N-0073-0002.json') == 'FDA-2018-N-0073-0002'
+
+
+def test_is_document_beginning_good():
+    assert df.document_id_beginning_is_letter('AHRQ_FRDOC_0001-0036') is True
+
+
+def test_is_document_beginning_bad():
+    assert df.document_id_beginning_is_letter('9147_FRDOC_0001-0036') is False
+
+
+def test_is_document_ending_a_number():
+    assert df.document_id_ending_is_number('FDA-2018-N-0073-0002') is True
+
+
+def test_is_document_ending_a_number_special():
+    assert df.document_id_ending_is_number('AHRQ_FRDOC_0001-0036') is True
+
+
+def test_is_document_ending_a_word():
+    assert df.document_id_ending_is_number('FDA-2018-N-0073-Abcd') is False
+
+
+def test_is_document_ending_a_word_special():
+    assert df.document_id_ending_is_number('AHRQ_FRDOC_0001-WXyz') is False
+
+
+def test_id_matches_good():
+    assert df.document_id_matches_json_id(PATH + 'doc.FMCSA-1997-2350-21654.json', 'FMCSA-1997-2350-21654') is True
+
+
+def test_id_matches_bad():
+    assert df.document_id_matches_json_id(PATH + 'doc.FMCSA-1997-2350-21653.json', 'FMCSA-1997-2350-21653') is False
+
+
 def test_save_single_file_locally(workfile_tempdir, savefile_tempdir):
-    filename = "doc.FMCSA-1997-2350-21654.json"
-    full_path = "/FMCSA/FMCSA-1997-2350/FMCSA-1997-2350-21654/doc.FMCSA-1997-2350-21654.json"
+    filename = 'doc.FMCSA-1997-2350-21654.json'
+    full_path = '/FMCSA/FMCSA-1997-2350/FMCSA-1997-2350-21654/doc.FMCSA-1997-2350-21654.json'
 
     path = workfile_tempdir + '/' + filename
     with open(path, 'w') as f:
-        f.write("Stuff was written here")
+        f.write('Stuff was written here')
 
     df.save_single_file_locally(path, savefile_tempdir + '/')
     final_path = savefile_tempdir + full_path
 
     assert os.path.exists(final_path)
 
+
+def test_get_file_name():
+    assert df.get_file_name(PATH + 'doc.mesd-2018-234234-0001.json') == 'doc.mesd-2018-234234-0001.json'
