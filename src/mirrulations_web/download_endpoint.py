f1cd5e0893cce4dc41d7ced5d08eceb6a11aabea
Manny <stmde03@moravian.edu>
2019-03-28 23:43:49 -0400

    minor tweak, same as last request

--- src/mirrulations_web/download_endpoint.py
+++ src/mirrulations_web/download_endpoint.py
@@ -1,5 +1,5 @@
 from io import BytesIO
-from flask import Flask, send_file
+from flask import Flask, send_file, send_from_directory
 from flask import request, render_template
 import zipfile
 import mirrulations_web.download_processor as download_processor
@@ -10,16 +10,15 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # this block is only entered when the form is submitted
-        mf=BytesIO();
+        memoryfile=BytesIO()
         docid = request.form.get('DirId')
-
-        print(docid)
         ZIP = zipfile.ZipFile(download_processor.download_zip(docid))
-        mf.seek(0)
-        print(ZIP)
-        return send_file(mf, attachment_filename=docid + '.zip', as_attachment=True)
-    return render_template("index.html")
 
+        memoryfile.seek(0)
+        print(ZIP)
+        return send_file(memoryfile, attachment_filename=docid + '.zip', as_attachment=True)
+    
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
