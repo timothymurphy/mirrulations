d3109d88f4b1f2b090bdd1a4473545051bd0d8c1
Lauren Mateo <stlem04@moravian.edu>
2019-04-01 16:27:48 -0400

    changed test_api_call.py to be pep8 compliant

--- tests/mirrulations_core_tests/test_api_call.py
+++ tests/mirrulations_core_tests/test_api_call.py
@@ -18,36 +18,30 @@ def test_happy_path(mock_req):
 
 
 def test_exception_thrown(mock_req):
-    mock_req.get(add_api_key(base_url),status_code=300)
+    mock_req.get(add_api_key(base_url), status_code=300)
     with pytest.raises(TemporaryException):
         call(add_api_key(base_url))
 
 
 def test_api_count_zero(mock_req):
-        mock_req.get(add_api_key(base_url), status_code=429)
-        with pytest.raises(ApiCountZeroException):
-            call(add_api_key(base_url))
+    mock_req.get(add_api_key(base_url), status_code=429)
+    with pytest.raises(ApiCountZeroException):
+        call(add_api_key(base_url))
 
 
 def test_api_permanent_exception(mock_req):
-        mock_req.get(add_api_key(base_url), status_code=404)
-        with pytest.raises(PermanentException):
-            call(add_api_key(base_url))
+    mock_req.get(add_api_key(base_url), status_code=404)
+    with pytest.raises(PermanentException):
+        call(add_api_key(base_url))
 
 
 def test_invalid_key_gives_permanent_exception(mock_req):
-        mock_req.get(add_api_key(base_url), status_code=403)
-        with pytest.raises(PermanentException):
-            call(add_api_key(base_url))
+    mock_req.get(add_api_key(base_url), status_code=403)
+    with pytest.raises(PermanentException):
+        call(add_api_key(base_url))
+
 
 def test_generic_500_failure(mock_req):
     mock_req.get(add_api_key(base_url), status_code=500)
     with pytest.raises(PermanentException):
         call(add_api_key(base_url))
-
-
-
-
-
-
-
