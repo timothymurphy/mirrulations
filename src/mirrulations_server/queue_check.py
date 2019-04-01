006d71761d45bf71d2fc5257f26b416ed0ed77d9
Alex Haug <stahh02@moravian.edu>
2019-04-01 15:22:42 -0400

    server queue check PEP8 compliant

--- src/mirrulations_server/queue_check.py
+++ src/mirrulations_server/queue_check.py
@@ -3,11 +3,12 @@ from mirrulations_server.redis_manager import RedisManager
 
 
 def queue_check(r):
-    return r.get_all_items_in_progress_no_lock() , r.get_all_items_in_queue_no_lock()
+    return r.get_all_items_in_progress_no_lock(), \
+           r.get_all_items_in_queue_no_lock()
 
 
 if __name__ == '__main__':
     r = RedisManager(redis.Redis())
-    progress,queue = queue_check(r)
+    progress, queue = queue_check(r)
     print(progress)
     print(queue)
