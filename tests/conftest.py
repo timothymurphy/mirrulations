3f4924ec789749648bc0dd853dd14a8a08dcc2fd
edwharbison <edwharbison@gmail.com>
2019-04-01 15:08:02 -0400

    Make test config not be a file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,17 @@
+import mock
+import pytest
 import random
 import string
-import json
-import os
-
-CONFIG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../config.json')
 
 
-def pytest_addoption(parser):
-    parser.addoption('--makeconfig', action='store', default=(not os.path.exists(CONFIG_PATH)))
+@pytest.fixture(scope='session', autouse=True)
+def mock_config():
+    fake_config_dictionary = {
+        'ip': '80.80.80.80',
+        'port': '8080',
+        'key': ''.join(random.choices(string.ascii_letters + string.digits, k=40)),
+        'client_id': ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    }
 
-
-def pytest_sessionstart(session):
-    if session.config.getoption('makeconfig'):
-        with open(CONFIG_PATH, 'wt') as file:
-            file.write(json.dumps({
-                'ip': '0.0.0.0',
-                'port': '8080',
-                'key': ''.join(random.choices(string.ascii_letters + string.digits, k=40)),
-                'client_id': ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-            }, indent=4))
-            file.close()
-
-
-def pytest_sessionfinish(session):
-    if session.config.getoption('makeconfig'):
-        os.remove(CONFIG_PATH)
+    with mock.patch('mirrulations_core.config.read_value', side_effect=lambda v: fake_config_dictionary[v]) as f:
+        yield f
