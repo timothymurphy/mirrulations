5428cdd408b1f25caaa81a00c0e4631f0bdfb427
Alex Haug <stahh02@moravian.edu>
2019-04-01 15:29:25 -0400

    server work generator PEP8 compliant

--- src/mirrulations_server/docs_work_gen.py
+++ src/mirrulations_server/docs_work_gen.py
@@ -10,9 +10,12 @@ from mirrulations_core.mirrulations_logging import logger
 
 def monolith():
     """
-    Runs the script. This is one monolithic function (aptly named) as the script just needs to be run; however, there is a certain
-    point where I need to break out of the program if an error occurs, and I wasn't sure how exactly sys.exit() would work and whether
-    or not it would mess with things outside of / calling this script, so I just made one giant method so I can return when needed.
+    Runs the script. This is one monolithic function (aptly named)
+    as the script just needs to be run; however, there is a certain
+    point where I need to break out of the program if an error occurs,
+    and I wasn't sure how exactly sys.exit() would work and whether
+    or not it would mess with things outside of / calling this script,
+    so I just made one giant method so I can return when needed.
     :return:
     """
     url_base = "https://api.data.gov/regulations/v3/documents.json?rpp=1000"
@@ -23,9 +26,11 @@ def monolith():
     if regulations_key != "":
         # Gets number of documents available to download
         try:
-            record_count = requests.get("https://api.data.gov/regulations/v3/documents.json?api_key=" + regulations_key
-                                        + "&countsOnly=1").json()["totalNumRecords"]
-        except:
+            record_count = requests.get(
+                "https://api.data.gov/regulations/v3/documents.json?api_key="
+                + regulations_key
+                + "&countsOnly=1").json()["totalNumRecords"]
+        except Exception:
             logger.error('Error occured with API request')
             print("Error occurred with docs_work_gen regulations API request.")
             return 0
@@ -33,7 +38,8 @@ def monolith():
         # Gets the max page we'll go to; each page is 1000 documents
         max_page_hit = record_count // 1000
 
-        # This loop generates lists of URLs, sending out a job and writing them to the work server every 1000 URLs.
+        # This loop generates lists of URLs, sending out a job and
+        # writing them to the work server every 1000 URLs.
         # It will stop and send whatever's left if we hit the max page limit.
         while current_page < max_page_hit:
             url_list = []
@@ -46,8 +52,11 @@ def monolith():
                 if current_page == max_page_hit:
                     break
 
-            # Makes a JSON from the list of URLs and send it to the queue as a job
-            docs_work = [''.join(random.choices(string.ascii_letters + string.digits, k=16)), "docs", url_list]
+            # Makes a JSON from the list of URLs and send
+            # it to the queue as a job
+            docs_work = [''.join(
+                random.choices(string.ascii_letters + string.digits, k=16)),
+                "docs", url_list]
             r.add_to_queue(endpoints.generate_json(docs_work))
     else:
         print("No API Key!")
