2900b80fe4941ea8d13528efe841432916316064
Manny <stmde03@moravian.edu>
2019-04-03 15:14:02 -0400

    The index.html file has been changed to send info as a file, which allows for a render template to be used in the flask endpoint. The download endpoint is a complete implementation of the mirrulations website, allowing for the downloading of files from the database

--- src/mirrulations_web/download_endpoint.py
+++ src/mirrulations_web/download_endpoint.py
@@ -1,7 +1,5 @@
-from io import BytesIO
-from flask import Flask, send_file, send_from_directory
+from flask import Flask, send_file
 from flask import request, render_template
-import zipfile
 import mirrulations_web.download_processor as download_processor
 
 app = Flask(__name__)
@@ -10,15 +8,11 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':  # this block is only entered when the form is submitted
-        memoryfile=BytesIO()
         docid = request.form.get('DirId')
-        ZIP = zipfile.ZipFile(download_processor.download_zip(docid))
+        return send_file(download_processor.download_zip(docid), attachment_filename=docid + '.zip', as_attachment=True)
 
-        memoryfile.seek(0)
-        print(ZIP)
-        return send_file(memoryfile, attachment_filename=docid + '.zip', as_attachment=True)
-    
     return render_template("index.html")
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
