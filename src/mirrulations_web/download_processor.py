f7ac0d333a479219a15fa3c7e80da8d02eb89d5d
Alex Haug <stahh02@moravian.edu>
2019-04-03 13:49:14 -0400

    web: removed  download processor

--- src/mirrulations_web/download_processor.py
+++ src/mirrulations_web/download_processor.py
@@ -1,14 +1,13 @@
-import os
 import mirrulations_web.dir_search as ds
 import mirrulations_web.directory_zip as dz
 
-HOME_REGULATION_PATH = "/regulations-data/"
+HOME_REGULATION_PATH = '/regulations-data/'
 
 
 def download_zip(document_id, download_path=HOME_REGULATION_PATH):
     zip_path = ds.search_for_document_in_directory(document_id, download_path)
-    if zip_path != "":
+    if zip_path != '':
         zipfile = dz.zip_directory(document_id, zip_path)
         return zipfile.filename
     else:
-        return ""
+        return ''
