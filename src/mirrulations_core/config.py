3ee45a61ce03224adb585a325236b7a9825c2d17
edwharbison <edwharbison@gmail.com>
2019-04-03 14:58:38 -0400

    Split config setup

--- src/mirrulations_core/config.py
+++ src/mirrulations_core/config.py
@@ -1,6 +1,23 @@
 import json
-from mirrulations_core.mirrulations_logging import logger
 import os
+import random
+import requests
+import string
+
+from mirrulations_core.mirrulations_logging import logger
+
+CONFIG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '../../.config/config.json')
+
+connection_error_string = 'Unable to connect!\n' \
+                          'We weren\'t able to connect to regulations.gov.\n' \
+                          'Please try again later.'
+invalid_key_error_string = 'Invalid API key!\n' \
+                           'Your API key is invalid.\n' \
+                           'Please visit\n' \
+                           'https://regulationsgov.github.io/developers/\n' \
+                           'for an API key.'
+successful_login_string = 'Success!\n' \
+                          'You are successfully logged in.'
 
 
 def read_value(value):
@@ -10,8 +27,7 @@ def read_value(value):
     :return: Value read from the JSON
     """
     try:
-        configurationpath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../.config/config.json")
-        contents = json.loads(open(configurationpath, "r").read())
+        contents = json.loads(open(CONFIG_PATH, "r").read())
         result = contents[value]
     except FileNotFoundError:
         logger.error('File Not Found Error')
@@ -27,3 +43,55 @@ def read_value(value):
         return None
     else:
         return result
+
+
+def verify_api_key(api_key):
+    try:
+        r = requests.get('https://api.data.gov/regulations/v3/documents.json?api_key='
+                         + api_key)
+    except requests.ConnectionError:
+        print(connection_error_string)
+        exit()
+    else:
+        if r.status_code == 403:
+            print(invalid_key_error_string)
+            exit()
+        elif r.status_code > 299 and r.status_code != 429:
+            print(connection_error_string)
+            exit()
+        print(successful_login_string)
+
+
+def client_config_setup():
+
+    api_key = input('API Key:\n')
+    verify_api_key(api_key)
+
+    client_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+    ip = input('IP:\n')
+    port = input('Port:\n')
+
+    with open(CONFIG_PATH, 'wt') as file:
+        file.write(json.dumps({
+            'api_key': api_key,
+            'client_id': client_id,
+            'ip': ip,
+            'port': port
+        }, indent=4))
+        file.close()
+
+
+def server_config_setup():
+
+    api_key = input('API Key:\n')
+    verify_api_key(api_key)
+
+    with open(CONFIG_PATH, 'wt') as file:
+        file.write(json.dumps({
+            'api_key': api_key,
+        }, indent=4))
+        file.close()
+
+
+def web_config_setup():
+    pass
