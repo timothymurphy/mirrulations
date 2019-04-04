a4ced53a628722334eeabe4f4d5cdf2f079a659a
edwharbison <edwharbison@gmail.com>
2019-04-04 10:25:10 -0400

    Split parser to other function

--- src/mirrulations_core/__main__.py
+++ src/mirrulations_core/__main__.py
@@ -6,19 +6,23 @@ from mirrulations_server.__main__ import main as server_main
 from mirrulations_web.__main__ import main as web_main
 
 CONFIG_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                           '../../.config/config.json')
+                           '../../.config/config.json')
 
 
-def main():
-
+def parse_args():
     parser = argparse.ArgumentParser(prog='mirrulations')
     parser.add_argument('enum', help='client/server/web')
     parser.add_argument('-c', '--config',
-                        action='store_true', help='force config setup')
+                        action='store_true',
+                        help='force config setup')
     args = vars(parser.parse_args())
-
     enum = args['enum']
-    do_config_setup = args['config'] or not os.path.exists(CONFIG_PATH)
+    do_config_setup = args['config'] or os.path.exists(CONFIG_PATH)
+    return enum, do_config_setup
+
+
+def main():
+    enum, do_config_setup = parse_args()
 
     if enum == 'client':
         client_main(do_config_setup)
