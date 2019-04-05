738bd18831627c596c4368f85eeb60bdc035f5ae
murphyt <sttam09@moravian.edu>
2019-04-05 15:34:43 -0400

    mocked out single docs job

--- tests/mirrulations_acceptance_tests/dummy_server.py
+++ tests/mirrulations_acceptance_tests/dummy_server.py
@@ -11,7 +11,29 @@ def default():
 
 @app.route('/get_work')
 def get_work():
-    pass
+    logger.warning('Successful API Call: %s', 'get_work: get_work')
+    if len(request.args) != 1:
+        logger.error('Incorrect number of parameters')
+        return 'Parameter Missing', 400
+    client_id = request.args.get('client_id')
+    if client_id is None:
+        logger.warning('Exception: %s',
+                       'get_work: BadParameterException, '
+                       'client id was none')
+        logger.error('Error - no client ID')
+        return 'Bad Parameter', 400
+    job_id = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                         for _ in range(16))
+    type = "docs"
+    data = ["https://api.data.gov/regulations/v3/documents.json?rpp=5&po=0"]
+    converted_json = {
+        'job_id': job_id,
+        'type': type,
+        'data': data,
+        'version': version
+    }
+    json_info = json.dumps(converted_json)
+    return json_info
 
 
 @app.route('/return_docs', methods=['POST'])
