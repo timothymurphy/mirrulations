f85e90520e93c0d97739aeddeebcbaa6fed835a7
murphyt <sttam09@moravian.edu>
2019-04-16 11:24:30 -0400

    Test for job in queue not in progress

--- tests/mirrulations_acceptance_tests/test_server.py
+++ tests/mirrulations_acceptance_tests/test_server.py
@@ -36,3 +36,14 @@ def test_when_two_jobs_in_db_returned_by_get_work(client):
     assert b'{"job_id": "3456", "type": "docs", "data": ["Url2"], "version": "0.5"}' == result_two.data
     assert rm.does_job_exist_in_progress('1234')
     assert rm.does_job_exist_in_progress('3456')
+
+def test_when_two_jobs_in_db_return_one_by_get_work(client):
+    rm = RedisManager()
+    rm.add_to_queue(json.dumps({'job_id': "1234", 'type': "docs", 'data': ["Url1"], 'version': "0.5"}))
+    rm.add_to_queue(json.dumps({'job_id': "3456", 'type': "docs", 'data': ["Url2"], 'version': "0.5"}))
+
+    result = client.get('/get_work?client_id=asdf')
+
+    assert b'{"job_id": "1234", "type": "docs", "data": ["Url1"], "version": "0.5"}' == result.data
+    assert rm.does_job_exist_in_progress('1234')
+    assert rm.does_job_exist_in_progress('3456') is False
