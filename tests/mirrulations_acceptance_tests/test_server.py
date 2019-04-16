9f43ac0d7815d2c0db0dd15fb291e17736f0bc25
murphyt <sttam09@moravian.edu>
2019-04-16 10:39:31 -0400

    First fully working test for get_work endpoint done

--- tests/mirrulations_acceptance_tests/test_server.py
+++ tests/mirrulations_acceptance_tests/test_server.py
@@ -5,6 +5,7 @@ import mirrulations_server.endpoints
 import fakeredis
 from mirrulations_server.redis_manager import RedisManager
 from unittest import mock
+import json
 
 @pytest.fixture
 def client():
@@ -17,20 +18,20 @@ def test_root_gives_empty_json(client):
     assert b'{}' == result.data
 
 
-def mock_redis_server():
-    redis_server = fakeredis.fakeredis()
-    mock_redis_manager = RedisManager(redis_server)
-    mock_redis_manager.add_to_queue('jsonstring')
-    return mock_redis_manager
+# def mock_redis_server():
+#     redis_server = fakeredis.fakeredis()
+#     mock_redis_manager = RedisManager(redis_server)
+#     mock_redis_manager.add_to_queue('jsonstring')
+#     return mock_redis_manager
 
 
-@mock.patch('mirrulations_server.endpoints.redis_server', side_effect=mock_redis_server)
+# @mock.patch('mirrulations_server.endpoints.redis_server', side_effect=mock_redis_server)
 def test_when_one_job_in_db_then_job_returned_by_get_work(client):
-
-    # mock out the Redis DB
-    # swap out our redis_server for the production one
-    # mock.patch('mirrulations_server.endpoints.redis_server', return_value=mock_redis_manager)
-
+    rm = RedisManager()
+    rm.add_to_queue(json.dumps({'job_id': "1234",'type': "docs",'data': ["Url1"],'version': "0.5"}))
+ 
     result = client.get('/get_work?client_id=asdf')
 
-    assert b'{}' == result.data
+    assert b'{"job_id": "1234", "type": "docs", "data": ["Url1"], "version": "0.5"}' == result.data
+    assert rm.does_job_exist_in_progress('1234')
+
