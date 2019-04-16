ad4d8709d51bdbf163510b5f410b44234291defb
murphyt <sttam09@moravian.edu>
2019-04-16 11:22:03 -0400

    Second test for multiple jobs in queue

--- tests/mirrulations_acceptance_tests/test_server.py
+++ tests/mirrulations_acceptance_tests/test_server.py
@@ -24,3 +24,15 @@ def test_when_one_job_in_db_then_job_returned_by_get_work(client):
     assert rm.does_job_exist_in_progress('1234')
 
 
+def test_when_two_jobs_in_db_returned_by_get_work(client):
+    rm = RedisManager()
+    rm.add_to_queue(json.dumps({'job_id': "1234", 'type': "docs", 'data': ["Url1"], 'version': "0.5"}))
+    rm.add_to_queue(json.dumps({'job_id': "3456", 'type': "docs", 'data': ["Url2"], 'version': "0.5"}))
+
+    result = client.get('/get_work?client_id=asdf')
+    result_two = client.get('/get_work?client_id=asdf')
+
+    assert b'{"job_id": "1234", "type": "docs", "data": ["Url1"], "version": "0.5"}' == result.data
+    assert b'{"job_id": "3456", "type": "docs", "data": ["Url2"], "version": "0.5"}' == result_two.data
+    assert rm.does_job_exist_in_progress('1234')
+    assert rm.does_job_exist_in_progress('3456')
