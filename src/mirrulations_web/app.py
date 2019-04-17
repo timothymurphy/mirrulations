bc1535bb6720491c36e7ecbaf25526fa93285ee9
Alex Haug <stahh02@moravian.edu>
2019-04-17 15:34:12 -0400

    Fix's web problem of crashing from bad ids

--- src/mirrulations_web/app.py
+++ src/mirrulations_web/app.py
@@ -1,18 +1,21 @@
 from flask import Flask, send_file
 from flask import request, render_template
 import mirrulations_web.download_processor as download_processor
+import mirrulations_web.dir_search as dir_search
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # this block is only entered when the form is submitted
     if request.method == 'POST':
         docid = request.form.get('DirId')
-        return send_file(download_processor.download_zip(docid),
-                         attachment_filename=docid +
-                         '.zip', as_attachment=True)
+        path = dir_search.search_for_document_in_directory(docid)
+        if path is not '':
+            return send_file(download_processor.download_zip(docid),
+                             attachment_filename=docid +
+                             '.zip', as_attachment=True)
 
     return render_template("index.html")
 
